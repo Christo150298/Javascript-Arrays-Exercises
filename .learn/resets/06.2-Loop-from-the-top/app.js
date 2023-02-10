8ee98e447d81b240b76b5e4bb0db090ab52e0b42
Christopher Álvarez <christopherjavier1998@gmail.com>
2023-02-10 15:53:07 +0000

    exercise 6.4

--- .learn/resets/06.2-Loop-from-the-top/app.js
+++ .learn/resets/06.2-Loop-from-the-top/app.js
@@ -1,9 +1,7 @@
 let mySampleArray = [3423,5,4,47889,654,8,867543,23,48,56432,55,23,25,12];
 
-const lastPos = mySampleArray.length -1 ;
-const stopAt = 0
 
-for(let i = lastPos; i >= stopAt; i = i - 1)
+for(let i = 0; i<mySampleArray.length; i = i + 1)
 {
 	console.log(mySampleArray[i]);
 }
